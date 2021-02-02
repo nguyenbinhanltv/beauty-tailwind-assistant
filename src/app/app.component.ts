1a1fd9fc1774d005fedc635d828d92c2bad3315e
Nguyen Binh An <nguyenbinhanltv@gmail.com>
2021-02-03 04:09:15 +0700

    config tailwindcss + alan ai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'beauty-tailwind-assistant';
+
+  alanBtnInstance;
+
+  constructor() {
+    this.alanBtnInstance = alanBtn({
+      key: '6e2f30a591a971a8b161db583bbed7582e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: () => {
+      },
+
+      // onConnectionStatus: async (status) => {
+      //   if (status === 'authorized') {
+      //     await this.alanBtnInstance.activate();
+      //     this.alanBtnInstance.playText('Hey, this is Alan');
+      //     this.alanBtnInstance.deactivate();
+      // }
+      // }
+    });
+  }
 }
